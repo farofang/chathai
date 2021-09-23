13467959e7b2a747040a4184d1b64d88ecd5b107
farofang <31241949+farofang@users.noreply.github.com>
2021-09-24 00:35:54 +0700

    tf-idf - read document

--- quiz1/tf-idf/producer-harry.py
+++ quiz1/tf-idf/producer-harry.py
@@ -1,24 +1,39 @@
 import time
 from json import dumps
 from kafka import KafkaProducer
+import re
 
 # producer - aws
 producer = KafkaProducer(bootstrap_servers=['ec2-13-229-46-113.ap-southeast-1.compute.amazonaws.com:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 
-file = open('quiz1/tf-idf/demo.txt', 'r')
-# file = open('quiz1/tf-idf/book.txt', 'r')
+# file = open('quiz1/tf-idf/demo.txt', 'r')
+file = open('quiz1/tf-idf/book.txt', 'r')
 Lines = file.readlines()
-i = 1
-lines_count = len(Lines)
-print('Line Total:', len(Lines))
+page = 1
+
+
+def replace_str(string):
+    string = re.sub('\W', ' ', string)
+    string = string.replace('    ', ' ')
+    string = string.replace('   ', ' ')
+    string = string.replace('  ', ' ').strip()
+    return string
+
+
+content = ""
 for data in Lines:
-    print('Line no: {} from {}'.format(i, lines_count))
-    data = str(i) + '|' + data
-    i += 1
-    sendMsg = data.encode().decode('utf-8').strip('\n')
-    producer.send('chathai-streams-harry-tf-idf-input', sendMsg)
-    time.sleep(1)
+    if len(re.sub('\W', '', data)) > 1:
+        if data.startswith('Page |'):
+            sendMsg = str(page) + '|' + content
+            page += 1
+            sendMsg = sendMsg
+            print(sendMsg)
+            producer.send('chathai-streams-harry-tf-idf-input', sendMsg)
+            content = ""
+            # time.sleep(1)
+        else:
+            content += replace_str(data).encode().decode('utf-8').strip('\n') + " "
 
 producer.flush()
